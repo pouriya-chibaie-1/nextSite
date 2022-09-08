3023f887b28ff3d4aa8d0a236a2ea484704c0e04
pouriya-chibaie-dev <85185561+pouriya-chibaie-dev@users.noreply.github.com>
2022-09-08 09:32:28 +0430

    add font Vazir

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,38 @@
+import style from '../styles/contact.module.scss'
+import TextField from '@mui/material/TextField';
 const ContactUs = () => {
     return (<>
- <h1>Contact Us</h1>
+<div className={style.container}>
+<div className={style.header}></div>
+<div className={style.content}>
+<div className={style.divInContent}>
+<div className={style.left}>
+<h1>یک پیام بگذارید
+</h1>
+<div>   
+<TextField   className={style.textField} inputProps={{style:{textAlign:"center"}}}label="نام و نام خانوادگی" variant="standard" />
+    <TextField className={style.textField} inputProps={{style:{textAlign:"center"}}} label="شماره تماس" variant="standard" />
+</div>
+<div>
+    <TextField className={style.textField} inputProps={{style:{textAlign:"center"}}} label="ایمیل" variant="standard" />
+    <TextField className={style.textField} inputProps={{style:{textAlign:"center"}}} label="موضوع" variant="standard" /> 
+</div>
+<div>
+<TextField
+          id="outlined-multiline-flexible"
+          label="پیام"
+          multiline
+          variant="standard"
+          rows={2}
+fullWidth        />
+</div>
+<button className={style.submitBtn}>ارسال پیام</button>
+</div>
+<div className={style.right}></div>
+</div>
+
+</div>
+</div>
     </>  );
 }
  
