ea679a832200c1f4403f26c22b7a36f353122d41
pouriya chibaie <pouriya.chibaie.dev@gmail.com>
2022-09-10 17:01:37 +0430

    rightSide contact.js finished

--- pages/contact.js
+++ pages/contact.js
@@ -1,39 +1,45 @@
 import style from '../styles/contact.module.scss'
 import TextField from '@mui/material/TextField';
-const ContactUs = () => {
-    return (<>
-<div className={style.container}>
-<div className={style.header}></div>
-<div className={style.content}>
-<div className={style.divInContent}>
-<div className={style.left}>
-<h1>یک پیام بگذارید
-</h1>
-<div>   
-<TextField   className={style.textField} inputProps={{style:{textAlign:"center"}}}label="نام و نام خانوادگی" variant="standard" />
-    <TextField className={style.textField} inputProps={{style:{textAlign:"center"}}} label="شماره تماس" variant="standard" />
-</div>
-<div>
-    <TextField className={style.textField} inputProps={{style:{textAlign:"center"}}} label="ایمیل" variant="standard" />
-    <TextField className={style.textField} inputProps={{style:{textAlign:"center"}}} label="موضوع" variant="standard" /> 
-</div>
-<div>
-<TextField
-          id="outlined-multiline-flexible"
-          label="پیام"
-          multiline
-          variant="standard"
-          rows={2}
-fullWidth        />
-</div>
-<button className={style.submitBtn}>ارسال پیام</button>
-</div>
-<div className={style.right}></div>
-</div>
+import { makeStyles } from '@mui/styles';
 
-</div>
-</div>
-    </>  );
+const useStyles = makeStyles({
+  root: {
+    "& .MuiInputBase-input": { fontFamily: "Vazir", color: "black", fontSize: "13px",position:"relative" },
+    "& .MuiFormLabel-root": { fontFamily: "Vazir", fontSize: "13px", width:"100%"},
+    "& label.Mui-focused":{position: "absolute",right: "-25%",textAlign:"right",width:"100%"}
+  }
+});
+const ContactUs = () => {
+  const classes = useStyles();
+
+  return (<>
+    <div className={style.container}>
+      <div className={style.header}>
+        <div className={style.divOpacity}></div>
+      </div>
+      <div className={style.content}>
+        <div className={style.divInContent}>ّ
+          <div className={style.left}>
+            <h1>یک پیام بگذارید
+            </h1>
+            <div className={style.divContainInput}>
+              <TextField className={classes.root} style={{width:"45%"}} 
+               label="نام و نام خانوادگی" variant="standard"/>
+              <TextField className={classes.root} style={{width:"45%"}}label="شماره تماس"  variant="standard"/>
+            </div>
+            <div className={style.divContainInput}>
+              <TextField className={classes.root}style={{width:"45%"}} label="ایمیل"ص variant="standard"/>
+              <TextField className={classes.root} style={{width:"45%"}}label="موضوع" variant="standard"/>
+            </div>
+            <div className={style.divTextArea}><TextField variant="standard" rows={2} className={classes.root} fullWidth label="موضوع"multiline /></div>
+            <button className={style.submitBtn}>ارسال پیام</button>
+          </div>
+          <div className={style.right}></div>
+        </div>
+
+      </div>
+    </div>
+  </>);
 }
- 
+
 export default ContactUs;
