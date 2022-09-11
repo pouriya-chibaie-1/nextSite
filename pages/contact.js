01ced73dd7b6772b9ec1e4d5d5f2b0271e454f59
pouriya chibaie <pouriya.chibaie.dev@gmail.com>
2022-09-11 17:16:51 +0430

    responsive contact

--- pages/contact.js
+++ pages/contact.js
@@ -1,16 +1,14 @@
 import style from '../styles/contact.module.scss'
 import TextField from '@mui/material/TextField';
-import { makeStyles } from '@mui/styles';
+import { makeStyles } from '@mui/styles'
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
-import PlaceIcon from '@mui/icons-material/Place';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Icon } from '@mui/material';
+import TelegramIcon from '@mui/icons-material/Telegram';
 const useStyles = makeStyles({
   root: {
-    "& .MuiInputBase-input": { fontFamily: "Vazir", color: "black", fontSize: "13px",position:"relative" },
-    "& .MuiFormLabel-root": { fontFamily: "Vazir", fontSize: "13px", width:"100%",},
-    "& label.Mui-focused":{textAlign:"right",width:"100%"},
+    "& .MuiInputBase-input": { fontFamily: "Vazir ", color: "black", fontSize: "13px", position: "relative" },
+    "& .MuiFormLabel-root": { fontFamily: "Vazir ", fontSize: "13px", width: "100%", },
+    "& label.Mui-focused": { fontFamily: "Vazir ", textAlign: "right", width: "100%" },
     "& label": {
       transformOrigin: "top right",
       right: 0,
@@ -19,8 +17,19 @@ const useStyles = makeStyles({
 });
 
 const ContactUs = () => {
+  const [fullName, setFullName] = useState("")
+  const [phone, setPhone] = useState("")
+  const [email, setEmail] = useState("")
+  const [subject, setSubject] = useState("")
+  const [message, setMessage] = useState("")
   const classes = useStyles();
-
+  useEffect(() => {
+    console.log(`fullname : ${fullName}  
+phone :${phone}
+email : ${email} 
+subject: ${subject} 
+ message:${message}`);
+  }, [fullName, phone, email, subject, message])
   return (<>
     <div className={style.container}>
       <div className={style.header}>
@@ -32,21 +41,30 @@ const ContactUs = () => {
             <h1>یک پیام بگذارید
             </h1>
             <div className={style.divContainInput}>
-              <TextField className={classes.root} style={{width:"45%"}} 
-               label="نام و نام خانوادگی" variant="standard"/>
-              <TextField className={classes.root} style={{width:"45%"}}label="شماره تماس"  variant="standard"/>
+              <TextField onChange={(e) => setFullName(e.target.value)} className={`${classes.root} ${style.inputContact}`} 
+                label="نام و نام خانوادگی" variant="standard" />
+              <TextField onChange={(e) => setPhone(e.target.value)} className={`${classes.root} ${style.inputContact}`}  label="شماره تماس" variant="standard" />
             </div>
             <div className={style.divContainInput}>
-              <TextField className={classes.root}style={{width:"45%"}} label="ایمیل" variant="standard"/>
-              <TextField className={classes.root} style={{width:"45%"}}label="موضوع" variant="standard"/>
+              <TextField onChange={(e) => setEmail(e.target.value)} className={`${classes.root} ${style.inputContact}`}  label="ایمیل" variant="standard" />
+              <TextField onChange={(e) => setSubject(e.target.value)} className={`${classes.root} ${style.inputContact}`}  label="موضوع" variant="standard" />
             </div>
-            <div className={style.divTextArea}><TextField variant="standard" rows={2} className={classes.root} fullWidth label="موضوع"multiline /></div>
+            <div className={style.divTextArea}><TextField onChange={(e) => setMessage(e.target.value)} variant="standard" rows={2} className={classes.root} fullWidth label="پیام" multiline /></div>
             <button className={style.submitBtn}>ارسال پیام</button>
           </div>
           <div className={style.right}>
-            <div> <span>اطلاعات تماس </span><span >موقعیت نقشه</span> </div>
+            <div className={style.divIconRight}><h2 >اطلاعات تماس</h2> <h2>نقشه موقعیت</h2></div>
+            <div className={style.divIconRight}><img src="/call.svg" /><h4 >  <a href="tel:+989382960927">09382960927</a></h4>  </div>
+            <div className={style.divIconRight}><img src='/location.svg' /><h4 >تهران شهر قدس خیابان سرقنات پاساژ<br /> میلاد واحد 84</h4></div>
+            <div className={style.divIconRight}><img src='/mail.svg' /><h4 ><a href="mailto:pouriya.chibaie.dev@gmail.com">pouriya.chibaie.dev@gmail.com</a></h4></div>
+            <div className={style.divIconRight}><img src='/web.svg' /><h4 ><a href="https:ekaseb.com">www.ekaseb.com</a></h4></div>
+            <div className={style.socialMediaDiv}>
+              <Image src='/whatsapp.svg'  width={35} height={35} />
+              <Image src='/instagram.svg' width={35} height={35}/>
+              <Image src='/linkdin.svg' width={35} height={35}/>
+              <TelegramIcon sx={{color:"white",width:"40px",height:"40px"}}/>
+            </div>
           </div>
-         <PlaceIcon/>
 
         </div>
 
