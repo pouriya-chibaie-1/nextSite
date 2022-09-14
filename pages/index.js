dde981a6c9538bf9e0210705e75949e85cece4fd
pouriya chibaie <pouriya.chibaie.dev@gmail.com>
2022-09-14 17:01:15 +0430

    starting main page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,122 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { makeStyles } from '@mui/styles'
+// const useStyles = makeStyles({
+//   root: {
+//     "& .MuiButtonBase-root":{fontFamily:"Vazir"}
+//   }
+// });
 
-export default function Home() {
+
+
+
+
+
+
+//MuiButtonBase-root
+const drawerWidth = 240;
+const navItems = ['خانه', 'درباره ما', 'تماس با ما'];
+
+function App(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' ,fontFamily:"Vazir"}}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        MUI
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
   return (
-    <div className={styles.container}>
-     <h1>in the name of god  </h1>
-     <h2>home page  </h2>
-    </div>
-  )
+    <Box sx={{ display: 'flex' ,fontFamily:"Vazir"}}>
+      <AppBar component="nav" sx={ {"& .MuiButtonBase-root":{fontFamily:"Vazir"},"& .MuiBox-root":{direction:"rtl"}}}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+           EKaseb.ir
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: '#fff' }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box component="main" sx={{ p: 3 }}>
+       
+      </Box>
+    </Box>
+  );
 }
+
+App.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default App;
--- pages/select.js
+++ pages/select.js
@@ -1,8 +1,11 @@
 import { DataGrid } from '@mui/x-data-grid';
 import { useEffect, useState } from 'react';
 import { makeStyles } from '@mui/styles'
-
-import { createTheme, ThemeProvider } from "@mui/material";
+import Radio from "@mui/material/Radio";
+import { Button, createTheme, ThemeProvider } from "@mui/material";
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import style from '../styles/select.module.scss'
+import { height } from '@mui/system';
 
 const myTheme = createTheme({
   components: {
@@ -18,62 +21,165 @@ const myTheme = createTheme({
     }
   }
 });
-const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
-];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: 24 },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: 1, firstName: "sales", lastName: "Obrien" },
+  { id: 2, firstName: "Kate", lastName: "Miles" },
+  { id: 3, firstName: "Kim", lastName: "Jimenez" },
+  { id: 4, firstName: "Juana", lastName: "Thornton" },
+  { id: 5, firstName: "Prashant", lastName: "Jangam" },
+  { id: 6, firstName: "sales", lastName: "Obrien" },
+  { id: 7, firstName: "Kate", lastName: "Miles" },
+  { id: 8, firstName: "Kim", lastName: "Jimenez" },
+  { id: 9, firstName: "Juana", lastName: "Thornton" },
+  { id: 10, firstName: "Prashant", lastName: "Jangam" },
+  { id: 11, firstName: "sales", lastName: "Obrien" },
+  { id: 12, firstName: "Kate", lastName: "Miles" },
+  { id: 13, firstName: "Kim", lastName: "Jimenez" },
+  { id: 14, firstName: "Juana", lastName: "Thornton" },
+  { id: 15, firstName: "Prashant", lastName: "Jangam" },
+  { id: 16, firstName: "sales", lastName: "Obrien" },
+  { id: 17, firstName: "Kate", lastName: "Miles" },
+  { id: 18, firstName: "Kim", lastName: "Jimenez" },
+  { id: 19, firstName: "Juana", lastName: "Thornton" },
+  { id: 20, firstName: "Prashant", lastName: "Jangam" },
+  { id: 21, firstName: "sales", lastName: "Obrien" },
+  { id: 22, firstName: "Kate", lastName: "Miles" },
+  { id: 23, firstName: "Kim", lastName: "Jimenez" },
+  { id: 24, firstName: "Juana", lastName: "Thornton" },
+  { id: 25, firstName: "Prashant", lastName: "Jangam" },
+  { id: 26, firstName: "sales", lastName: "Obrien" },
+  { id: 27, firstName: "Kate", lastName: "Miles" },
+  { id: 28, firstName: "Kim", lastName: "Jimenez" },
+  { id: 29, firstName: "Juana", lastName: "Thornton" },
+  { id: 30, firstName: "Prashant", lastName: "Jangam" },
+  { id: 31, firstName: "sales", lastName: "Obrien" },
+  { id: 32, firstName: "Kate", lastName: "Miles" },
+  { id: 33, firstName: "Kim", lastName: "Jimenez" },
+  { id: 34, firstName: "Juana", lastName: "Thornton" },
+  { id: 35, firstName: "Prashant", lastName: "Jangam" },
+  { id: 36, firstName: "sales", lastName: "Obrien" },
+  { id: 37, firstName: "Kate", lastName: "Miles" },
+  { id: 38, firstName: "Kim", lastName: "Jimenez" },
+  { id: 39, firstName: "Juana", lastName: "Thornton" },
+  { id: 40, firstName: "Prashant", lastName: "Jangam" },
+  { id: 41, firstName: "sales", lastName: "Obrien" },
+  { id: 42, firstName: "Kate", lastName: "Miles" },
+  { id: 43, firstName: "Kim", lastName: "Jimenez" },
+  { id: 44, firstName: "Juana", lastName: "Thornton" },
+  { id: 45, firstName: "Prashant", lastName: "Jangam" },
+  { id: 46, firstName: "sales", lastName: "Obrien" },
+  { id: 47, firstName: "Kate", lastName: "Miles" },
+  { id: 48, firstName: "Kim", lastName: "Jimenez" },
+  { id: 49, firstName: "Juana", lastName: "Thornton" },
+  { id: 50, firstName: "Prashant", lastName: "Jangam" },
+  { id: 51, firstName: "sales", lastName: "Obrien" },
+  { id: 52, firstName: "Kate", lastName: "Miles" },
+  { id: 53, firstName: "Kim", lastName: "Jimenez" },
+  { id: 54, firstName: "Juana", lastName: "Thornton" },
+  { id: 55, firstName: "Prashant", lastName: "Jangam" },
+  { id: 56, firstName: "sales", lastName: "Obrien" },
+  { id: 57, firstName: "Kate", lastName: "Miles" },
+  { id: 58, firstName: "Kim", lastName: "Jimenez" },
+  { id: 59, firstName: "Juana", lastName: "Thornton" },
+  { id: 60, firstName: "Prashant", lastName: "Jangam" },
+  { id: 61, firstName: "sales", lastName: "Obrien" },
+  { id: 62, firstName: "Kate", lastName: "Miles" },
+  { id: 63, firstName: "Kim", lastName: "Jimenez" },
+  { id: 64, firstName: "Juana", lastName: "Thornton" },
+  { id: 65, firstName: "Prashant", lastName: "Jangam" },
+  { id: 66, firstName: "sales", lastName: "Obrien" },
+  { id: 67, firstName: "Kate", lastName: "Miles" },
+  { id: 68, firstName: "Kim", lastName: "Jimenez" },
+  { id: 69, firstName: "Juana", lastName: "Thornton" },
+  { id: 70, firstName: "Prashant", lastName: "Jangam" },
+  { id: 71, firstName: "sales", lastName: "Obrien" },
+  { id: 72, firstName: "Kate", lastName: "Miles" },
+  { id: 73, firstName: "Kim", lastName: "Jimenez" },
+  { id: 74, firstName: "Juana", lastName: "Thornton" },
+  { id: 75, firstName: "Prashant", lastName: "Jangam" },
+  { id: 76, firstName: "sales", lastName: "Obrien" },
+  { id: 77, firstName: "sales", lastName: "Obrien" },
+  { id: 78, firstName: "Kim", lastName: "Jimenez" },
+  { id: 79, firstName: "Juana", lastName: "Thornton" },
+  { id: 80, firstName: "Prashant", lastName: "Jangam" },
 ];
+let radioChecked = [rows[0].id];
 
 export default function Select() {
-  const [selectedRow,setSelectedRow]=useState()
-  const [selectedRowIndex,setSelectedRowIndex]=useState(1)
-  useEffect(()=>{
-    if(selectedRow){
-setSelectedRowIndex(selectedRow.row.id)
-      console.log(selectedRow.row);
-      console.log("id = ",selectedRow.row.id);
-    }
-  },[selectedRow])
+  const [selectionModel, setSelectionModel] = useState(radioChecked);
+  const [rowPerPage, setRowPerPage] = useState(10);
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  radioChecked = selectionModel;
 
+  const selectedRow = rows.filter((item) => {
+    return item.id === selectionModel[0];
+  });
+  const columns = [
+    {
+      field: "radiobutton",
+      headerName: "",
+      width: 100,
+      sortable: false,
+      renderCell: (params) => (
+        <Radio checked={radioChecked[0] === params.id} value={params.id} />
+      )
+    },
+    {
+      field: "id",
+      headerName: "ID"
+    },
+    {
+      field: "firstName",
+      headerName: "First Name",
+      width: 150
+    },
+    {
+      field: "lastName",
+      headerName: "Last Name",
+      width: 150
+    },
+    {
+      field: "Print",
+      renderCell: (cellValues) => {
+        return (
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => {
+            setDrawerOpen(true)
+  
+            }}
+          >
+            انتخاب
+          </Button>
+        );
+      },}
+  ];
   return (
-    <div style={{ height: 400, width: '100%' }}>
+    <div className="App">
       <ThemeProvider theme={myTheme}>
-      <DataGrid
+
+      <DataGrid  sx={{height:"400px"}}
         rows={rows}
         columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        // isRowSelectable={(e)=>{setSelectedRow(e.target.value)}}
-        onRowClick={(rowData)=>setSelectedRow(rowData)}
+        autoHeight
+        selectionModel={selectionModel}
+        onSelectionModelChange={(newSelectionModel) => {
+          setSelectionModel(newSelectionModel);
+        }}
+        rowsPerPage={10}
+        rowsPerPageOptions={[10,20,30,40,100]}
         />
+        <SwipeableDrawer open={drawerOpen} onClose={()=>setDrawerOpen(false)} anchor="bottom" width={100} >
+          <div className={style.drawerSelect}>
+
+        You have selected: {selectedRow[0].firstName} {selectedRow[0].lastName}
+          </div>
+        </SwipeableDrawer>
         </ThemeProvider>
+      <div style={{ marginTop: "40px" }}>
+      </div>
     </div>
   );
 }
