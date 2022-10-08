7ebd856918dd9a3b5349b7db37d663fcdec18b9b
pouriya chibaie <pouriya.chibaie.dev@gmail.com>
2022-10-08 17:17:12 +0330

    create footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,10 +2,38 @@ import React from 'react';
 import styles from '../styles/footer.module.scss';
 
 const Footer = () => {
-   
+   const style={textAlign:"right",display:"flex",flexDirection:"column",justifyContent:'center'}
+
     return (<>
     <div className={styles.container}>
-        FOOTER
+        <div style={{width:"90%",height:"100%",margin:"auto",direction:"rtl",display:"grid",gridTemplateColumns:"repeat(4,1fr)"}}>
+            <div style={{textAlign:"right",display:"flex",flexDirection:"column",justifyContent:'center'}}>
+                <ul>
+                    <ol>خانه</ol>
+                    <ol>امکانات</ol>
+                    <ol>فروشگاه ها</ol>
+                    <ol>خرید و اجاره </ol>
+                </ul></div>
+            <div style={style}><ul>
+           
+           <ol>معرفی</ol>
+            <ol>باشگاه مشتریان</ol>
+            <ol>اخبار و اطلاعیه ها</ol>
+            <ol>تماس با ما</ol>
+            </ul></div>
+            <div style={style}>
+                <ul>
+                    <ol>تهران شهرستان قدس ، خیابان آزادی پاساژ میلاد</ol>
+                    <ol>اطلاعات تماس:09382960927</ol>
+                    <ol>پست الکترونیکی:pouriya.chibaie.dev@gmail.com</ol>
+                    <ol></ol>
+                </ul>
+            </div>
+            <div style={style}>
+                <img src='/milladmap.png'/>
+            </div>
+        </div>
+        <img src='/'/>
     </div>
     </>  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../components/Header';
 import "swiper/css/pagination";
 import "swiper/css/navigation";
@@ -8,23 +8,41 @@ import { Pagination, Navigation } from "swiper";
 import styles from "../styles/Home.module.css"
 import Footer from '../components/Footer';
 import dynamic from 'next/dynamic';
+import useDeviceSize from '../components/useDeviceSize';
+import { useState } from 'react';
 const App = () => {
+  const [sliderPerView,setSliderPerView] = useState(1);
+  const s = useDeviceSize()
+console.log(s);
+useEffect(()=>{
+if(s[0]>=500){
+  setSliderPerView(2)
+}
+if(s[0]>=800){
+  setSliderPerView(3)
+}
+if(s[0]<=500){
+  setSliderPerView(1)
+}
+},[s])
   const BasicTabs = dynamic(() => import('../components/TabPageHome'), { ssr: false })
   return (<>
+<div style={{overflowX:"hidden"}}>
 
     <Header />
     <Swiper
-
-      spaceBetween={50}
+     
+      spaceBetween={0}
       loop={true}
       pagination={{ clickable: true, }}
       navigation={true}
       style={{ width: "100%", height: "31.5vw" }}
-      className={`mySwiper ${styles.homeSider}`}
+      className={`${styles.homeSider}`}
       slidesPerView={1}
       modules={[Pagination, Navigation]}
+
     >
-      <SwiperSlide style={{ background: "red" }}>Slide 1</SwiperSlide>
+      <SwiperSlide style={{ background: "red"}}>Slide 1</SwiperSlide>
       <SwiperSlide style={{ background: "aqua" }}>Slide 2</SwiperSlide>
       <SwiperSlide style={{ background: "green" }}>Slide 3</SwiperSlide>
       <SwiperSlide style={{ background: "blue" }}>Slide 4</SwiperSlide>
@@ -35,22 +53,26 @@ const App = () => {
       <div className={styles.gridItem}></div>
       <div className={styles.gridItem}></div>
     </div>
-    <div>
-      <Swiper
+    <div >
+      <div style={{width:"100%" ,direction:"rtl" , marginBottom:"22px"}}>
 
-        spaceBetween={50}
+      <h1 style={{textAlign:"right" , marginRight:"35px" ,width:"240px", borderBottom:"4px solid black"}}>اخبار و اطلاعیه ها</h1>
+      </div>
+      <Swiper
+     slidesPerView={sliderPerView}
+    spaceBetween={50}
         loop={true}
         navigation={true}
         style={{ width: "86%", height: "21.5vw",margin:"auto" ,display:"flex",justifyContent:"space-between"}}
-        className={`mySwiper ${styles.homeSider}`}
-        slidesPerView={3}
       >
-        <SwiperSlide style={{ background: "white",textAlign:"center",border:"1px solid black" }}>Slide 1</SwiperSlide>
-        <SwiperSlide style={{ background: "white",textAlign:"center",border:"1px solid black" }}>Slide 2</SwiperSlide>
-        <SwiperSlide style={{ background: "white",textAlign:"center",border:"1px solid black" }}>Slide 3</SwiperSlide>
+        <SwiperSlide style={{ background: "white",textAlign:"center",border:"1px solid black",backgroundColor:"red" }} >Slide 1</SwiperSlide>
+        <SwiperSlide style={{ background: "white",textAlign:"center",border:"1px solid black",backgroundColor:"green" }}>Slide 2</SwiperSlide>
+        <SwiperSlide style={{ background: "white",textAlign:"center",border:"1px solid black",backgroundColor:"blue" }}>Slide 3</SwiperSlide>
       </Swiper>
     </div>
     <Footer />
+</div>
+
   </>);
 }
 
